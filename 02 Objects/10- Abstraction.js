de9a0cca2ac92df1b0ccf01bbaa73ec01e5fda69
Faig-Abaszada <faik.abaszada@gmail.com>
2023-07-02 14:32:55 +0400

    added some additions

--- 02 Objects/10- Abstraction.js
+++ 02 Objects/10- Abstraction.js
@@ -1,4 +1,6 @@
 // 10 - Abstraction
+// Mesele sadece bezi property ve methodlarin primave olmamasinin tehlukesinden izah vermekdir
+// Novbeti dersde bunun qarshisini almagi gosterecek.
 
 function Circle(radius) {
   this.radius = radius;
@@ -10,6 +12,9 @@ function Circle(radius) {
 
   this.draw = function draw() {
     this.computeOptimumLocation(); // We define the method and call it in the draw method. THIS MAYBE NOT A GOOD PRACTICE
+                                   // Maybe this Method should only be called inside draw method, but now we can call it from outside
+                                   // Belelikle biz bu metodun yalniz draw icinde cagrila bilecek shekilde etmek ucun onu kenardan cagrilmasinin
+                                   // qarshisini almaliyiq.
     console.log("Draw");
   };
 }
@@ -18,7 +23,7 @@ const circle = new Circle(10);
 
 // In some situation not all the members should be accessible to the consumer or client of this object.
 // For example if we set it to false it is going to completely mess up the object.
-circle.computeOptimumLocation = false; 
+circle.computeOptimumLocation = false;
 
 // Maybe the computeOptimumLocation method, and defaultLocation property should be hidden, they could be implementation details, and should be hidden from the consumer of this object
 // Suppose we only need to call the computeOptimumLocation from inside the draw method. We will implement this in the next lecture.
