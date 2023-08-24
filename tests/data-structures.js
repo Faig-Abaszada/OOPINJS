013389be97b0652590d4128c6119f7e9792e51b2
Faig <faik.abaszada@gmail.com>
2023-08-24 09:39:55 +0400

    added movie list

---
+++ tests/data-structures.js
@@ -0,0 +1,22 @@
+let movies = [
+  "1. Moby Dick; Or, The Whale by Herman Melville (2513)",
+  "2. Romeo and Juliet by William Shakespeare (2433)",
+  "3. A Room with a View by E. M. Forster (2077)",
+  "4. Middlemarch by George Eliot (2054)",
+  "5. The Complete Works of William Shakespeare by William Shakespeare (1945)",
+  "6. Little Women; Or, Meg, Jo, Beth, and Amy by Louisa May Alcott (1913)",
+  "7. Pride and Prejudice by Jane Austen (1897)",
+  "8. The Blue Castle: a novel by L. M. Montgomery (1840)",
+  "9. The Enchanted April by Elizabeth Von Arnim (1827)",
+  "10. The Adventures of Ferdinand Count Fathom — Complete by T. Smollett (1728)",
+  "11. Cranford by Elizabeth Cleghorn Gaskell (1696)",
+  "12. The Expedition of Humphry Clinker by T. Smollett (1680)",
+  "13. The Adventures of Roderick Random by T. Smollett (1673)",
+  "14. History of Tom Jones, a Foundling by Henry Fielding (1664)",
+  "15. Twenty years after by Alexandre Dumas and Auguste Maquet (1617)",
+  "16. My Life — Volume 1 by Richard Wagner (1610)",
+  "17. Frankenstein; Or, The Modern Prometheus by Mary Wollstonecraft Shelley (1341)",
+  "18. Alice's Adventures in Wonderland by Lewis Carroll (1017)",
+  "19. Queer questions and ready replies : by S. Grant Oliphant (802)",
+  "20. Dracula by Bram Stoker (734)"
+];
